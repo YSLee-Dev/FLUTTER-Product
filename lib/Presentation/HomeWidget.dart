cb926e7c59f1e2333f1e6d12052f70a2776755d3
YSLee-Dev <ysleedev01@gmail.com>
2024-10-14 10:54:33 +0900

    Feat(Home): 기본 Home 구성

--- lib/Presentation/HomeWidget.dart
+++ lib/Presentation/HomeWidget.dart
@@ -8,8 +8,52 @@ class HomeWidget extends StatefulWidget {
 }
 
 class _HomeWidgetState extends State<HomeWidget> {
+  List<String> _temp = ["1", "2", "1", "2", "1", "2", "1", "2"];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          scrolledUnderElevation: 0,
+          title: Text("Home"),
+        ),
+        body: Container(
+          color: Colors.white,
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.amber),
+                        onPressed: () {},
+                        child: Text("리스트 보기")),
+                  ),
+                  SizedBox(width: 20),
+                  Expanded(
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue),
+                        onPressed: () {},
+                        child: Text("격자 보기")),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: _temp.length,
+                    itemBuilder: (context, int) {
+                      return Text("${int}");
+                    }),
+              )
+            ],
+          ),
+        ));
   }
 }
