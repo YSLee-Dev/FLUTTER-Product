7e7543b689da1d984acb873e17fd91cd11813c05
YSLee-Dev <ysleedev01@gmail.com>
2024-10-15 11:24:50 +0900

    Feat(Home): 리스트, 격자 보기 기능 추가 및 디자인 추가

--- lib/Presentation/HomeViewModel.dart
+++ lib/Presentation/HomeViewModel.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_product/Dependency/IProductManager.dart';
+import 'package:flutter_product/Dependency/ProductManager.dart';
 import 'package:flutter_product/Model/ProductModel.dart';
+import 'package:flutter_product/Network/INetworkManager.dart';
 import 'package:provider/provider.dart';
+import '../Network/NetworkManager.dart';
 
 class HomeViewModel with ChangeNotifier {
   bool nowListShowTapped = true;
-  //List<ProductModel> nowProductModelList = [];
-  List<String> temp = ["1", "2", "1", "2", "1", "2", "1", "2"];
+  int nowPage = 1;
+  final IProductManager _productManager;
+
+  List<ProductModel> nowProductModelList = [];
+
+  HomeViewModel({IProductManager? productManager})
+   : _productManager = productManager ?? ProductManager() {
+    requestProductList(1, 10);
+  }
 
   void nowTappedIsList(bool isList) {
     nowListShowTapped = isList;
+    notifyListeners();
+  }
+
+  void requestProductList(int page, int count) async {
+    nowProductModelList = await _productManager.requestMainProduct(1, 10);
     notifyListeners();
   }
 }
--- lib/Presentation/HomeWidget.dart
+++ lib/Presentation/HomeWidget.dart
@@ -16,7 +16,6 @@ class _HomeWidgetState extends State<HomeWidget> {
   Widget build(BuildContext context) {
     final provider = context.watch<HomeViewModel>();
 
-
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
@@ -33,45 +32,208 @@ class _HomeWidgetState extends State<HomeWidget> {
                   Expanded(
                     child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
-                            backgroundColor: provider.nowListShowTapped ? Colors.black : Colors.black26
-                        ),
+                            backgroundColor: provider.nowListShowTapped
+                                ? Colors.black
+                                : Colors.black26),
                         onPressed: () {
                           provider.nowTappedIsList(true);
                         },
-                        child: Text("리스트 보기", style: TextStyle(color: Colors.white),)),
+                        child: Text(
+                          "리스트 보기",
+                          style: TextStyle(color: Colors.white),
+                        )),
                   ),
                   SizedBox(width: 20),
                   Expanded(
                     child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
-                            backgroundColor: !provider.nowListShowTapped ? Colors.black : Colors.black26
-                        ),
+                            backgroundColor: !provider.nowListShowTapped
+                                ? Colors.black
+                                : Colors.black26),
                         onPressed: () {
                           provider.nowTappedIsList(false);
                         },
-                        child: Text("격자 보기", style: TextStyle(color: Colors.white),)),
+                        child: Text(
+                          "격자 보기",
+                          style: TextStyle(color: Colors.white),
+                        )),
                   ),
                 ],
               ),
               SizedBox(
                 height: 30,
               ),
-              if(provider.nowListShowTapped)
-              Expanded(
-                child: ListView.builder(
-                    itemCount: provider.temp.length,
-                    itemBuilder: (context, int) {
-                      return Text("${int}");
-                    }),
-              ),
-              if(!provider.nowListShowTapped)
+              if (provider.nowProductModelList.isEmpty)
+                SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: CircularProgressIndicator(),
+                ),
+              if (provider.nowListShowTapped)
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: provider.nowProductModelList.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                            padding: EdgeInsets.only(top: 10, bottom: 10),
+                            child: Row(
+                              children: [
+                                Container(
+                                  height: 120,
+                                  decoration: BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(15)),
+                                      border: Border.all(
+                                          color: Colors.black38, width: 1)),
+                                  child: Image.network(
+                                      '${provider.nowProductModelList[index].thumbnail}',
+                                      loadingBuilder:
+                                          (context, child, loadingProgress) {
+                                    if (loadingProgress == null) {
+                                      return SizedBox(
+                                          width: 120,
+                                          height: 120,
+                                          child: child);
+                                    }
+                                    ;
+                                    return SizedBox(
+                                      width: 120,
+                                      height: 120,
+                                      child: CircularProgressIndicator(),
+                                    );
+                                  }),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                    child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      '${provider.nowProductModelList[index].title}',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                      maxLines: 3,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          '${provider.nowProductModelList[index].price}',
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold),
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Text(
+                                          '${provider.nowProductModelList[index].tags?.first ?? ""}',
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.normal),
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                )
+                                )
+                              ],
+                            )
+                        );
+                      }
+                      ),
+                ),
+              if (!provider.nowListShowTapped)
                 Expanded(
                   child: GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                      itemCount: provider.temp.length,
-                      itemBuilder: (context, int) {
-                        return Text("${int}");
-                      }),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
+                      itemCount: provider.nowProductModelList.length,
+
+                      itemBuilder: (context, index) {
+                        return Container(
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15)),
+                              border:
+                                  Border.all(color: Colors.black38, width: 1)),
+                          child: Stack(
+                            alignment: Alignment.bottomCenter,
+                            children: [
+                              Image.network(
+                                  '${provider.nowProductModelList[index].thumbnail}',
+                                  loadingBuilder:
+                                      (context, child, loadingProgress) {
+                                    if (loadingProgress == null) {
+                                      return child;
+                                    };
+                                    return CircularProgressIndicator();
+                                  }),
+                              Container(
+                                padding: EdgeInsets.all(5),
+                                height: 60,
+                                decoration: BoxDecoration(
+                                    color: Colors.black45,
+                                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15), bottomRight: Radius.circular(15))
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      '${provider.nowProductModelList[index].title}',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.bold
+                                      ),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        Spacer(),
+                                        Text(
+                                          '${provider.nowProductModelList[index].price}',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold),
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        SizedBox(width: 5),
+                                        Text(
+                                          '${provider.nowProductModelList[index].tags?.first ?? ""}',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.normal),
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        Spacer()
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      }
+                      ),
                 ),
             ],
           ),
