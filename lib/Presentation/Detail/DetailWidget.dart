2939daac0acb5dad193228abede4c32db9c43dd7
YSLee-Dev <ysleedev01@gmail.com>
2024-10-17 14:27:34 +0900

    Feat(Detail): 사진을 스크롤 하여 볼 수 있는 기능 추가

--- lib/Presentation/Detail/DetailWidget.dart
+++ lib/Presentation/Detail/DetailWidget.dart
@@ -12,12 +12,87 @@ class DetailWidget extends StatefulWidget {
 }
 
 class _DetailWidgetState extends State<DetailWidget> {
+  late final PageController _pageController;
+  bool imageWidgetLeftArrowisHideen = true;
+  bool imageWidgetRightArrowisHideen = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pageController = PageController();
+    _pageController.addListener (() {
+      if ((_pageController.page ?? 0.1) % 1.0 == 0.0) {
+        setState(() {
+          imageWidgetLeftArrowisHideen = _pageController.page == 0.0;
+          imageWidgetRightArrowisHideen = _pageController.offset >= _pageController.position.maxScrollExtent;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final _provider = context.watch<DetailViewModel>();
 
     return Scaffold(
-      appBar: AppBar(title: Text("${_provider.sendedDetailInfo.title}"),),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        scrolledUnderElevation: 0,
+        title: Text("Detail"),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(left: 20, right: 20),
+        color: Colors.white,
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 200,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  PageView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: _provider.sendedDetailInfo.images?.length ?? 0,
+                      controller: _pageController,
+                      itemBuilder: (context, index) {
+                        double screenWidth = MediaQuery.of(context).size.width;
+                        return Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              Image.network(
+                                  _provider.sendedDetailInfo.images![index], width: screenWidth, height: 200,
+                                  loadingBuilder: (context, child, loadingProgress) {
+                                    if(loadingProgress == null) {return child;}
+                                    return CircularProgressIndicator();
+                                  }
+                              ),
+                            ]
+                        );
+                      }
+                  ),
+                  if (!imageWidgetLeftArrowisHideen)
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text("◀", style: TextStyle(fontSize: 40, fontWeight: FontWeight.w600, color: Colors.black87),),
+                    ),
+                  if (!imageWidgetRightArrowisHideen)
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: Text("▶", style: TextStyle(fontSize: 40, fontWeight: FontWeight.w600, color: Colors.black87)),
+                    )
+                ],
+              )
+            )
+          ],
+        )
+      )
     );
   }
 }
