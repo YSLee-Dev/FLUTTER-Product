e377024841aea148ffe69aa86e737c4a6ef0649a
YSLee-Dev <ysleedev01@gmail.com>
2024-10-18 15:52:52 +0900

    Feat(Detail): Cart에 추가하는 버튼 및 수량을 알 수 있는 위젯 추가

--- lib/Presentation/Detail/DetailWidget.dart
+++ lib/Presentation/Detail/DetailWidget.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -64,151 +65,180 @@ class _DetailWidgetState extends State<DetailWidget> {
           action: () {
             Navigator.pop(context);
           },
-          widget: ListView(
+          widget: Stack(
+            alignment: Alignment.bottomCenter,
             children: [
-              SizedBox(
-                  height: 200,
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
+              ListView(
+                children: [
+                  SizedBox(
+                      height: 200,
+                      child: Stack(
+                        alignment: Alignment.bottomCenter,
+                        children: [
+                          PageView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: _provider.sendedDetailInfo.images?.length ?? 0,
+                              controller: _imagePageController,
+                              itemBuilder: (context, index) {
+                                return Stack(
+                                    alignment: Alignment.center,
+                                    children: [
+                                      Image.network(
+                                          _provider.sendedDetailInfo.images![index], width: _screenWidth, height: 200,
+                                          loadingBuilder: (context, child, loadingProgress) {
+                                            if(loadingProgress == null) {return child;}
+                                            return CircularProgressIndicator();
+                                          }
+                                      ),
+                                    ]
+                                );
+                              }
+                          ),
+                          if (!_imageWidgetLeftArrowisHideen)
+                            Container(
+                              alignment: Alignment.centerLeft,
+                              child: Text("◀", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600, color: Colors.black87),),
+                            ),
+                          if (!_imageWidgetRightArrowisHideen && (_provider.sendedDetailInfo.images?.length ?? 0) >= 2)
+                            Container(
+                              alignment: Alignment.centerRight,
+                              child: Text("▶", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600, color: Colors.black87)),
+                            )
+                        ],
+                      )
+                  ),
+                  SizedBox(height: 5,),
+                  Stack(
+                    alignment: Alignment.centerLeft,
                     children: [
-                      PageView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: _provider.sendedDetailInfo.images?.length ?? 0,
-                          controller: _imagePageController,
+                      Container(
+                        decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)), color: Colors.black12),
+                        width: _screenWidth,
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)), color: Colors.black87),
+                        width: _screenWidth - (_screenWidth / max(((_provider.sendedDetailInfo.images?.length ?? 1) - 1), 1) * max((((_provider.sendedDetailInfo.images?.length ?? 1) - 1) - _nowPage), 0)),
+                        height: 20,
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Stack(
+                    alignment: Alignment.centerLeft,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)), color: Color(0xfffcffcc),),
+                        width: 100,
+                        height: 50,
+                      ),
+                      Text("${_provider.sendedDetailInfo.title}", style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold), maxLines: 2, overflow: TextOverflow.ellipsis)
+                    ],
+                  ),
+                  Text('${_provider.sendedDetailInfo.brand ?? 'Depends on destination'}', style: TextStyle(fontSize: 16)),
+                  SizedBox(height: 20,),
+                  Container(
+                    height: 30,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: _provider.sendedDetailInfo.tags?.length ?? 0,
+                        itemBuilder: (builder, index) {
+                          return Row(
+                            children: [
+                              Container(
+                                  decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)), color: Colors.black45),
+                                  alignment: Alignment.center,
+                                  padding: EdgeInsets.only(left: 15, right: 15),
+                                  child: Text("${_provider.sendedDetailInfo.tags![index]}", style: TextStyle(fontSize: 14, color: Colors.white))
+                              ),
+                              SizedBox(width: 10,)
+                            ],
+                          );
+                        }
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("${((_provider.sendedDetailInfo.price ?? 0.0) / ((100 - (_provider.sendedDetailInfo.discountPercentage ?? 0.0)) / 100)).toStringAsFixed(2)}\$",
+                        style: TextStyle(decoration: TextDecoration.lineThrough, fontSize: 17),
+                      ),
+                      Row(
+                        children: [
+                          Text('${_provider.sendedDetailInfo.discountPercentage}%', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23, color: Colors.redAccent,)),
+                          SizedBox(width: 10,),
+                          Text('${_provider.sendedDetailInfo.price}\$ ' , style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),),
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Text("Reviews →", style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),),
+                  SizedBox(height: 5,),
+                  Container(
+                      decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)), border: Border.all(color: Colors.black45, width: 1)),
+                      height: 60,
+                      child: PageView.builder(
+                          controller: _reviewsPageController,
+                          scrollDirection: Axis.vertical,
+                          itemCount: _provider.sendedDetailInfo.reviews?.length ?? 0,
+                          onPageChanged: (index) {
+                            _provider.nowShowingReviewIndexChnage(index);
+                          },
                           itemBuilder: (context, index) {
-                            return Stack(
-                                alignment: Alignment.center,
+                            return Container(
+                              width: _screenWidth,
+                              padding: EdgeInsets.only(left: 15, right: 15),
+                              height: 60,
+                              child: Row(
                                 children: [
-                                  Image.network(
-                                      _provider.sendedDetailInfo.images![index], width: _screenWidth, height: 200,
-                                      loadingBuilder: (context, child, loadingProgress) {
-                                        if(loadingProgress == null) {return child;}
-                                        return CircularProgressIndicator();
-                                      }
-                                  ),
-                                ]
+                                  Text('🧑🏻 ', style: TextStyle(fontSize: 20),),
+                                  Text('${_provider.sendedDetailInfo.reviews![index].name}', style: TextStyle(fontSize: 15),),
+                                  SizedBox(width: 10),
+                                  Flexible(child: Text('${_provider.sendedDetailInfo.reviews![index].comment}', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600), maxLines: 1, overflow: TextOverflow.ellipsis,))
+                                ],
+                              ),
                             );
                           }
-                      ),
-                      if (!_imageWidgetLeftArrowisHideen)
-                        Container(
-                          alignment: Alignment.centerLeft,
-                          child: Text("◀", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600, color: Colors.black87),),
-                        ),
-                      if (!_imageWidgetRightArrowisHideen && (_provider.sendedDetailInfo.images?.length ?? 0) >= 2)
-                        Container(
-                          alignment: Alignment.centerRight,
-                          child: Text("▶", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600, color: Colors.black87)),
-                        )
-                    ],
-                  )
-              ),
-              SizedBox(height: 5,),
-              Stack(
-                alignment: Alignment.centerLeft,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)), color: Colors.black12),
-                    width: _screenWidth,
-                    height: 20,
+                      )
                   ),
+                  SizedBox(height: 20,),
+                  Text("Product description", style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),),
+                  SizedBox(height: 5,),
                   Container(
-                    decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)), color: Colors.black87),
-                    width: _screenWidth - (_screenWidth / max(((_provider.sendedDetailInfo.images?.length ?? 1) - 1), 1) * max((((_provider.sendedDetailInfo.images?.length ?? 1) - 1) - _nowPage), 0)),
-                    height: 20,
-                  )
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)), border: Border.all(color: Colors.black45, width: 1)),
+                    child: Text("${_provider.sendedDetailInfo.description}", style: TextStyle(fontSize: 15),),
+                  ),
+                  SizedBox(height: 100,) // 여백
                 ],
               ),
-              SizedBox(height: 20,),
-              Stack(
-                alignment: Alignment.centerLeft,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)), color: Color(0xfffcffcc),),
-                    width: 100,
-                    height: 50,
-                  ),
-                  Text("${_provider.sendedDetailInfo.title}", style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold), maxLines: 2, overflow: TextOverflow.ellipsis)
-                ],
-              ),
-              Text('${_provider.sendedDetailInfo.brand ?? 'Depends on destination'}', style: TextStyle(fontSize: 16)),
-              SizedBox(height: 20,),
               Container(
-                height: 30,
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: _provider.sendedDetailInfo.tags?.length ?? 0,
-                    itemBuilder: (builder, index) {
-                      return Row(
-                        children: [
-                          Container(
-                              decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)), color: Colors.black45),
-                              alignment: Alignment.center,
-                              padding: EdgeInsets.only(left: 15, right: 15),
-                              child: Text("${_provider.sendedDetailInfo.tags![index]}", style: TextStyle(fontSize: 14, color: Colors.white))
-                          ),
-                          SizedBox(width: 10,)
-                        ],
-                      );
-                    }
-                ),
-              ),
-              SizedBox(height: 20),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("${((_provider.sendedDetailInfo.price ?? 0.0) / ((100 - (_provider.sendedDetailInfo.discountPercentage ?? 0.0)) / 100)).toStringAsFixed(2)}\$",
-                    style: TextStyle(decoration: TextDecoration.lineThrough, fontSize: 17),
-                  ),
-                  Row(
-                    children: [
-                      Text('${_provider.sendedDetailInfo.discountPercentage}%', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23, color: Colors.redAccent,)),
-                      SizedBox(width: 10,),
-                      Text('${_provider.sendedDetailInfo.price}\$ ' , style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),),
-                    ],
-                  )
-                ],
-              ),
-              SizedBox(height: 20,),
-              Text("Reviews →", style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),),
-              SizedBox(height: 5,),
-              Container(
-                decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)), border: Border.all(color: Colors.black45, width: 1)),
-                height: 60,
-                child: PageView.builder(
-                    controller: _reviewsPageController,
-                    scrollDirection: Axis.vertical,
-                    itemCount: _provider.sendedDetailInfo.reviews?.length ?? 0,
-                    onPageChanged: (index) {
-                        _provider.nowShowingReviewIndexChnage(index);
-                    },
-                    itemBuilder: (context, index) {
-                      return Container(
+                width: _screenWidth,
+                height: 90,
+                padding: EdgeInsets.only(top: 10),
+                decoration: BoxDecoration(color: Colors.white),
+                child: Column(
+                  children: [
+                    Text("Store: ${_provider.sendedDetailInfo.stock}", style: TextStyle( fontSize: 15, fontWeight: FontWeight.bold),),
+                    SizedBox(height: 10,),
+                    Expanded(child: Container(
                         width: _screenWidth,
-                        padding: EdgeInsets.only(left: 15, right: 15),
-                        height: 60,
-                        child: Row(
-                          children: [
-                            Text('🧑🏻 ', style: TextStyle(fontSize: 20),),
-                            Text('${_provider.sendedDetailInfo.reviews![index].name}', style: TextStyle(fontSize: 15),),
-                            SizedBox(width: 10),
-                            Text('${_provider.sendedDetailInfo.reviews![index].comment}', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),)
-                          ],
+                        child: ElevatedButton(
+                            style:  ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                            onPressed: () {
+                            print("CART INSERT BTN TAPPED");
+                          },
+                            child: Text("Add to Cart", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),)
                         ),
-                      );
-                    }
+                      )
+                    )
+                  ],
                 )
               ),
-              SizedBox(height: 20,),
-              Text("Product description", style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),),
-              SizedBox(height: 5,),
-              Container(
-                padding: EdgeInsets.all(15),
-                decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)), border: Border.all(color: Colors.black45, width: 1)),
-                child: Text("${_provider.sendedDetailInfo.description}", style: TextStyle(fontSize: 15),),
-              )
             ],
-          )
+          ),
       )
     );
   }
