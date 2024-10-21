791bde1c628920160e78d5bdead7875107b4c864
YSLee-Dev <ysleedev01@gmail.com>
2024-10-21 10:56:42 +0900

    Feat(Common): Common Main Widget이 BG컬러, Radius 값을 받고, 적용할 수 있는 로직 추가

--- lib/Component/CommonWidget/CommonMainWidget.dart
+++ lib/Component/CommonWidget/CommonMainWidget.dart
@@ -1,38 +1,46 @@
 import 'package:flutter/material.dart';
 
 class CommonMainWidget extends StatelessWidget {
+  final bool _isInScaffold;
   final Widget _widget;
   final String _title;
   final bool _isBackIconShow;
+  final Color _backGroundColor;
+  final BorderRadius _borderRadius;
   final Function()? _titleTappedAction;
 
-  CommonMainWidget({required String title, required bool isBackIconShow, Function()? action, required Widget widget})
-      : _widget = widget, _title = title, _isBackIconShow = isBackIconShow, _titleTappedAction = action;
+  CommonMainWidget({bool isInScaffold = true, required String title, required bool isBackIconShow, Color backgroundColor = Colors.white, BorderRadius borderRadius = BorderRadius.zero, Function()? action, required Widget widget})
+      : _isInScaffold = isInScaffold,  _widget = widget, _title = title, _isBackIconShow = isBackIconShow, _backGroundColor = backgroundColor, _borderRadius = borderRadius, _titleTappedAction = action;
+
+  Widget _createWidget() {
+   return Container(
+     decoration: BoxDecoration(borderRadius: _borderRadius, color: _backGroundColor),
+     padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+     child: Column(
+       crossAxisAlignment: CrossAxisAlignment.start,
+       children: [
+         GestureDetector(
+           onTap: () {
+             if (_titleTappedAction != null) {
+               _titleTappedAction!();
+             }
+           },
+           child: Text("${_isBackIconShow ? "← " : ""}${_title}", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),
+         ),
+         Expanded(child: _widget)
+       ],
+     ),
+   );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Container(
-            padding: EdgeInsets.only(left: 20, right: 20, top: 10),
-            color: Colors.white,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    if (_titleTappedAction != null) {
-                      _titleTappedAction!();
-                    }
-                  },
-                  child: Text("${_isBackIconShow ? "←" : ""} ${_title}", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),
-                ),
-                Expanded(child: _widget)
-              ],
-            ),
+    return _isInScaffold ?
+        Scaffold(
+          backgroundColor: _backGroundColor,
+          body: SafeArea(child: _createWidget()
           ),
         )
-    );
+        : _createWidget();
   }
 }
